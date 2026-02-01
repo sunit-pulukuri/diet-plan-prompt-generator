44262f811ece6c21b0b757edccdce3e939d9bbf5
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-02-01 12:42:40 +0530

    fix: add habits to initialState

--- src/state/initialState.js
+++ src/state/initialState.js
@@ -1,14 +1,22 @@
 export const initialState = {
   profile: {
-  age: "",
-  gender: "",
-  height: "",
-  heightUnit: "cm",
-  weight: "",
-  weightUnit: "kg",
-  trainingType: "",
-  activityLevel: "",
-  mealsPerDay: "",
-  budget: "",
-}
-}
+    age: "",
+    gender: "",
+    height: "",
+    heightUnit: "cm",
+    weight: "",
+    weightUnit: "kg",
+    trainingType: "",
+    activityLevel: "",
+    mealsPerDay: "",
+    budget: "",
+  },
+  habits: {
+    smoking: "",
+    alcohol: "",
+    sleep: "",
+    stress: "",
+    cookingTime: "",
+    spiceTolerance: ""
+  },
+};
