d7a45276f48af42c859d052a9c294dbcb409bd50
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2026-02-04 19:14:52 +0530

    feat: add instagram logo

--- src/components/WIzard.jsx
+++ src/components/WIzard.jsx
@@ -1,9 +1,20 @@
-export default function Wizard({ steps, currentStep, setCurrentStep, appState, setAppState, onReset }) {
+export default function Wizard({
+  steps,
+  currentStep,
+  setCurrentStep,
+  appState,
+  setAppState,
+  onReset,
+}) {
   const StepComponent = steps[currentStep];
 
   return (
     <div className="mt-8">
-      <StepComponent appState={appState} setAppState={setAppState} onReset={onReset} />
+      <StepComponent
+        appState={appState}
+        setAppState={setAppState}
+        onReset={onReset}
+      />
 
       <div className="flex justify-between items-center mt-10">
         <button
@@ -14,10 +25,26 @@ export default function Wizard({ steps, currentStep, setCurrentStep, appState, s
           Back
         </button>
 
-        <div>
-          ~ by {" "}
-          <a className="font-semibold underline underline-offset-4" href="https://instagram.com/sunit_pulz" target="_blank">
-            @sunit_pulz
+        <div className="flex gap-1 items-center justify-center">
+          
+          <a
+            className="font-semibold underline underline-offset-4 flex items-center gap-1"
+            href="https://instagram.com/sunit_pulz"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              className="size-4 mt-1"
+            >
+              <path
+                fill="#000"
+                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+              ></path>
+            </svg>
+            sunit_pulz
           </a>
         </div>
 
